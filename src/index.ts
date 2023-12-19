1dd6c2b6244fa1c4f102fc19e1ea053cbd5c2362
salino3 <programacionflavio@gmail.com>
2023-12-19 09:43:16 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// npm install express
+// npm install @types/express --save-dev
+// npm install sequelize sequelize-typescript mysql2
+// npm install @types/sequelize --save-dev
+// npm install nodemon --save-dev
+
+
+
+
+
+
+
