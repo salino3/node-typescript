256f41f0a36cac6c91942fb1ec24ab18fef31941
salino3 <programacionflavio@gmail.com>
2023-12-19 09:59:08 +0100

    add first endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,26 @@
 // npm install -g ts-node
 // npm install sequelize-cli --save-dev
 // npx sequelize-cli init
+// npm i dotenv
+
+import express from 'express';
+import dotenv from 'dotenv';
+import { Request, Response } from "express";
 
 
 
+const app = express();
 
+dotenv.config();
 
+const port = process.env.PORT;
+ 
+ 
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
