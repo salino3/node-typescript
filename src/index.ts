0b8fc6f6f641c145e1be8ffbbed1dbf5fd267a05
salino3 <programacionflavio@gmail.com>
2023-12-19 09:46:19 +0100

    add sequelize cli

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@
 // npm install sequelize sequelize-typescript mysql2
 // npm install @types/sequelize --save-dev
 // npm install nodemon --save-dev
-
-
+// npm install -g ts-node
+// npm install sequelize-cli --save-dev
+// npx sequelize-cli init
 
 
 
