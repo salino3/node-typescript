201c53cf92bcd8723d9c579c2e4761582d498e64
salino3 <programacionflavio@gmail.com>
2023-12-20 09:22:25 +0100

    add getOne function

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUser, deleteUser, getUsers, updateUser } from "../controllers/users";
+import { addUser, deleteUser, getOne, getUsers, updateUser } from "../controllers/users";
 
 const routerUsers = express.Router();
 
@@ -7,8 +7,10 @@ routerUsers.get("/users", getUsers);
 
 routerUsers.post("/users", addUser);
 
+routerUsers.get("/users/:id", getOne);
+
 routerUsers.put("/users/:id", updateUser);
 
 routerUsers.delete("/users/:id", deleteUser);
 
-export {routerUsers}
+export {routerUsers} 
