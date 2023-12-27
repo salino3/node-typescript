ece819a43b6daf43c04f37d258d64146241c53f0
salino3 <programacionflavio@gmail.com>
2023-12-27 13:24:06 +0100

    add simple front

---
+++ front/src/app.styles.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { theme } from "./themes";
+
+export const Main = styled.main`
+  background-color: ${theme.background.primary};
+  padding: ${theme.paddings.paddingApp};
+`;
