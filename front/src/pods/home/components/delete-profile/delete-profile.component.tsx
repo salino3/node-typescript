c6799eebb4c295f4485dbe8e03587d2c5d397c4a
salino3 <programacionflavio@gmail.com>
2024-01-01 10:10:00 +0100

    changing to add-user-form component

--- front/src/pods/home/components/delete-profile/delete-profile.component.tsx
+++ front/src/pods/home/components/delete-profile/delete-profile.component.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { FormField } from '@/common';
+import * as classes from './delete-profile.styles';
+
 
 export const DeleteProfile: React.FC = () => {
 
   return (
     <div>
         
-        
+        <form action="/users/:id">
+            {/* <FormField
+             name='email'
+             required
+             type='email'
+             nameValue={''}
+             pl='Insert your email..'
+              /> */}
+        </form>
     </div>
   )
 }
