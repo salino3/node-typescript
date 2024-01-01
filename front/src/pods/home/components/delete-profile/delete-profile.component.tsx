106cbac2926f2673fc96ae228293a0ffd54df095
salino3 <programacionflavio@gmail.com>
2024-01-01 10:32:10 +0100

    create delete form

--- front/src/pods/home/components/delete-profile/delete-profile.component.tsx
+++ front/src/pods/home/components/delete-profile/delete-profile.component.tsx
@@ -1,22 +1,54 @@
 import React from 'react';
-import { FormField } from '@/common';
+import { Button, FormField } from '@/common';
 import * as classes from './delete-profile.styles';
 
 
 export const DeleteProfile: React.FC = () => {
 
+    const [user, setUser] = React.useState<{email: string; password: string;}>({
+        email: "",
+        password: ""
+    });
+
+   const handleChange = (key: keyof {email: string; password: string;}) => (event: any) => {
+
+    const {value} = event.target;
+    setUser({...user, [key]: value});
+   };
+
+   const handleSubmit: React.FormEventHandler<HTMLFormElement> | undefined = (
+     event: React.FormEvent<HTMLFormElement>
+   ) => {
+     event.preventDefault();
+
+     alert(JSON.stringify(user));
+   };
+
   return (
-    <div>
-        
-        <form action="/users/:id">
-            {/* <FormField
-             name='email'
-             required
-             type='email'
-             nameValue={''}
-             pl='Insert your email..'
-              /> */}
-        </form>
+    <div className={classes.container}>
+        <h2>Delete Form</h2>
+      <form action="/users/:id" onSubmit={handleSubmit}>
+        <FormField
+          name="email"
+          required
+          type="email"
+          handleChange={handleChange("email")}
+          nameValue={user?.email}
+          pl="Insert your email.."
+        />
+        <FormField
+          name="password"
+          required
+          type="password"
+          handleChange={handleChange("password")}
+          nameValue={user?.password}
+          pl="Insert your password.."
+        />
+        <Button
+         text='Submit'
+         type='submit'
+          />
+      </form>
     </div>
-  )
+  );
 }
