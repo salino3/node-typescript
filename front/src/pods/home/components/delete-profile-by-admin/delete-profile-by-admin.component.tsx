ba93dacd30e2a44bd8ebb2a5e6a499e002e65618
salino3 <programacionflavio@gmail.com>
2024-01-06 10:09:17 +0100

    delete a user by admin

--- front/src/pods/home/components/delete-profile-by-admin/delete-profile-by-admin.component.tsx
+++ front/src/pods/home/components/delete-profile-by-admin/delete-profile-by-admin.component.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
-import * as classes from './delete-profile-by-admin.styles';
+import Axios from 'axios';
 import { Button, FormField } from '@/common';
+import * as classes from './delete-profile-by-admin.styles';
 
 
 export const DeleteProfileByAdmin: React.FC = () => {
 
-     const [user, setUser] = React.useState<{email: string; password: string;}>({
+     const [user, setUser] = React.useState<{id: string; email: string; password: string;}>({
+      id: "",
        email: "",
        password: "",
      });
 
      const handleChange =
-       (key: keyof { email: string; password: string }) => (event: any) => {
+       (key: keyof {id: string; email: string; password: string }) => (event: any) => {
          const { value } = event.target;
          setUser({ ...user, [key]: value });
-       };
+     };
+
+     const handleSubmit: React.FormEventHandler<HTMLFormElement> | undefined = (
+       event: React.FormEvent<HTMLFormElement>
+     ) => {
+       event.preventDefault();
+
+      const storedUserId = localStorage.getItem("my-identification-userId");
+
+     const token = document.cookie.replace(
+        new RegExp(`(?:(?:^|.*;\\s*)my-token-${storedUserId}\\s*=\\s*([^;]*).*$)|^.*$`),
+        "$1"
+     );
+
+
+      Axios.delete(
+        `${import.meta.env.VITE_APP_BASE_URL}/users/admin/${user.id}/${storedUserId}`,
+        {
+          data: user,
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          }
+        }
+      )
+      .then((res) => {
+         console.log(res);
+        })
+        .catch((error) => {
+          console.error("Error", error);
+        });
+     };
 
   return (
     <div className={classes.container}>
       <h2>Delete Form</h2>
-      <form action="/users/admin/:id/:idAdmin">
+      <form action="/users/admin/:id/:idAdmin" onSubmit={handleSubmit}>
+        <FormField
+          name="ID user"
+          required
+          type="text"
+          handleChange={handleChange("id")}
+          nameValue={user?.id}
+          pl="Insert the user ID.."
+        />{" "}
         <FormField
           name="email"
           required
