5277934ef3a240d8227785ee47f39c242ebfd339
salino3 <programacionflavio@gmail.com>
2024-01-14 12:11:51 +0100

    commit

--- front/src/pods/home/home.component.tsx
+++ front/src/pods/home/home.component.tsx
@@ -10,36 +10,9 @@ export const HomeComponent: React.FC = () => {
     <classes.Div>
       <Header />
       <h1>Home Page</h1>
-      <details className={classes.detailsHome}>
-        <summary>
-          <span>Create New Profile</span>
-        </summary>
-        <AddUserForm />
-      </details>
-      <details className={classes.detailsHome}>
-        <summary>All Users</summary>
-        <AllUsers />
-      </details>
-      <details className={classes.detailsHome}>
-        <summary>Login User</summary>
-        <LoginForm />
-      </details>
-      <details className={classes.detailsHome}>
-        <summary>Logout</summary>
-        <Logout />
-      </details>
-      <details className={classes.detailsHome}>
-        <summary>Get One User</summary>
-        <GetOneUser />
-      </details>
-      <details className={classes.detailsHome}>
-        <summary>Delete your profile</summary>
-        <DeleteProfile />
-      </details>
-      <details className={classes.detailsHome}>
-        <summary>Delete Profile By Admin</summary>
-        <DeleteProfileByAdmin />
-      </details>
+     
     </classes.Div>
   );
 }
+
+
